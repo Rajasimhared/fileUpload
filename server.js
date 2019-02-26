be63e62c75c4d7e0fd32c1d0492dbc8ea251b542
Rajasimha <rajasimha5599@gmail.com>
2019-02-26 09:49:42 +0530

    add css, file upload using formidable

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+var formidable = require('formidable');
 // Server configuration
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -10,9 +11,29 @@ fs.readFile('./index.html', function(err, html) {
   }
   const server = http
     .createServer(function(request, response) {
-      response.writeHeader(200, { 'Content-Type': 'text/html' });
-      response.write(html);
-      response.end();
+      if (request.url == '/fileupload') {
+        var form = new formidable.IncomingForm();
+        form.parse(request, function(err, fields, files) {
+          var oldpath = files.filetoupload.path;
+          var newpath =
+            '/home/hacker/Projects/fileUpload/' + files.filetoupload.name;
+          fs.rename(oldpath, newpath, function(err) {
+            if (err) throw err;
+            response.write('File uploaded and moved!');
+            response.end();
+          });
+        });
+      } else if (request.url == '/index.css') {
+        response.writeHeader(200, { 'Content-Type': 'text/css' });
+        var fileContents = fs.readFileSync('./index.css', { encoding: 'utf8' });
+        response.write(fileContents);
+        response.end();
+      } else {
+        response.writeHeader(200, { 'Content-Type': 'text/html' });
+        response.write(html);
+
+        response.end();
+      }
     })
     .listen(port, hostname, () => {
       console.log(`Server running at http://${hostname}:${port}/`);
